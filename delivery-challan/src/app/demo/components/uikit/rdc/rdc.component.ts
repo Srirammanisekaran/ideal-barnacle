e1d1ad6ec80cec6ac99641ab4a47c34e26c7928a
sriram <34161735+srirammani@users.noreply.github.com>
2023-10-18 12:33:04 +0000

    Pending changes exported from your codespace

--- delivery-challan/src/app/demo/components/uikit/rdc/rdc.component.ts
+++ delivery-challan/src/app/demo/components/uikit/rdc/rdc.component.ts
@@ -6,6 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DataService } from 'src/app/data.service';
 import { DatePipe } from '@angular/common';
 import { ToWords } from 'to-words';
+
 interface Code {
   code: number;
 }
@@ -23,24 +24,31 @@ export class RdcComponent implements OnInit{
   data: any;
   li:any;
   lis=[];
+  ven:any;
+  vendor=[];
+  vena:any;
+  venaddrs=[];
   selectedValue: any;
-  selectedData: any[] = [];
+  selectedData = [];
+  selected = [];
+  selectedSup = [];
+  selectedVenaddrs: any[] = [];
   dataOptions: any[] = [];
   vendorOptions: any[] = [];
   selectedOption: any[] = [];
   selectedVendor: any[] = [];
   companycode: Code[] | undefined;
   selectedCode: Code | undefined;
-  finyear: Year[] | undefined;
-  selectedyear: Year | undefined;
-  doctype: Doc[] | undefined;
+  finyear: Year[] =[];
+  selectedyear: Year | number;
+  doctype: Doc[] =[];
   selectedtype: Doc | undefined;
   filterdate: Date | undefined;
   invoicedata: any[] = [];
   netAmount: number = 0;
 constructor(private formBuilder: FormBuilder, private datePipe: DatePipe, private dataService: DataService, private _http: HttpClient) { }
-  
- 
+
+
   ngOnInit(): void {
 
     const apiUrl = 'https://my403075-api.s4hana.cloud.sap/sap/opu/odata/sap/API_OPLACCTGDOCITEMCUBE_SRV/A_OperationalAcctgDocItemCube'; // Replace with the actual API URL
@@ -55,10 +63,23 @@ constructor(private formBuilder: FormBuilder, private datePipe: DatePipe, privat
         this.li=response;
         this.lis = this.li.d.results;
         this.dataOptions = this.getUniqueOptions(this.lis);
-        this.vendorOptions = this.getUniqueVendor(this.lis);
-        console.log(this.lis);   
       });
-       
+      const vendorurl = 'https://my403075-api.s4hana.cloud.sap/sap/opu/odata/sap/API_BUSINESS_PARTNER/A_Supplier'; // Replace with the actual API URL 
+      this._http.get(vendorurl, { headers })
+      .subscribe((sup :any) => {
+        this.ven=sup;
+        this.vendor = this.ven.d.results;
+        this.vendorOptions = this.getUniqueVendor(this.vendor);
+        //console.log(this.vendor);   
+      });
+      const venaddrurl = 'https://my403075-api.s4hana.cloud.sap/sap/opu/odata/sap/YY1_APIBUSINESSPARTNERADDR_CDS/YY1_APIBusinessPartnerAddr'; // Replace with the actual API URL 
+      this._http.get(venaddrurl, { headers })
+      .subscribe((sup :any) => {
+        this.vena=sup;
+        this.venaddrs = this.vena.d.results;
+        //console.log(this.venaddrs);   
+      });
+  
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
     this.companycode = [
       { code: 1000 },
@@ -66,7 +87,7 @@ constructor(private formBuilder: FormBuilder, private datePipe: DatePipe, privat
       { code: 3000 }
   ];
   this.finyear = [
-    { year: 2022 },
+     { year: 2022 },
     { year: 2023 },
     { year: 2024 },
     { year: 2025 },
@@ -76,26 +97,56 @@ constructor(private formBuilder: FormBuilder, private datePipe: DatePipe, privat
     { year: 2029 },
     { year: 2030 }
 ];
+let currentYear = new Date().getFullYear();
+this.selectedyear = currentYear;
 this.doctype = [
   { type: 'KZ' }
-  
 ];
-  let today = new Date();
-        this.filterdate = today;
+
   }
   
+  getVendorAddrs(data : string){
+    
+    this.selectedVenaddrs = [];
+    //console.log(data);
+    const vendoraddrs = this.venaddrs.filter(obj => obj.BusinessPartner === data);
+    this.selectedVenaddrs = vendoraddrs;
+    const index = this.selectedVenaddrs.findIndex(x => x.BusinessPartner === data);
+    //console.log(this.selectedVenaddrs[index].OrganizationName1);
+    return this.selectedVenaddrs[index].OrganizationName1 +',\n'+ this.selectedVenaddrs[index].StreetName 
+    +',\n '+this.selectedVenaddrs[index].StreetPrefixName1+',\n'+this.selectedVenaddrs[index].StreetPrefixName2
+    +',\n'+ this.selectedVenaddrs[index].CityName+'-'+this.selectedVenaddrs[index].PostalCode;
+  }
   onOptionSelect(event: any) {
     this.selectedData = []; // Clear previous selection
-    if (this.selectedOption ) {
-      
-      // const selectedOptionData = this.lis.find(option => option.AccountingDocument === this.selectedOption);
-      const selectedOptionData = this.lis.filter(obj => obj.AccountingDocument === this.selectedOption);
-      this.selectedData = selectedOptionData;
-      console.log(this.selectedData);
-    }
+    //this.selected = []; // Clear previous selection
+    // console.log(this.selectedtype.type);
    
-  }
+    //console.log(styear);
+    if (this.selectedOption || this.selectedyear) {
+      var styear : string = String(this.selectedyear);
+      const selectedOptionData = this.lis.filter(obj => obj.AccountingDocument === this.selectedOption && obj.AccountingDocumentType === this.selectedtype.type && obj.FiscalYear === styear);
+      //console.log(selectedOptionData);
+      //this.selectedData = selectedOptionData;
+      const greatestAmount =   Math.min(...selectedOptionData.map(el => el.AmountInCompanyCodeCurrency));
+      var stamount : string = String(greatestAmount.toFixed(2));
 
+      const greatestAmountData = selectedOptionData.filter(item => item.AmountInCompanyCodeCurrency === stamount);
+      this.selectedData = greatestAmountData;
+     
+    }else if(this.selectedCode){
+
+    }
+  }
+  getsupplier(acdoc: number){
+    this.selectedSup = [];
+    const supplier = this.lis.filter(obj => obj.AccountingDocument === acdoc && obj.AccountingDocumentType === 'KZ' && obj.FiscalYear === '2023');
+    this.selectedSup = supplier;
+    //console.log(this.selectedSup);
+    const indexsup = this.selectedSup.findIndex(x => x.Supplier !== "");
+    //console.log(indexsup);
+    return this.getVendorAddrs(this.selectedSup[indexsup].Supplier);
+  }
   getUniqueOptions(data: any[]): any[] {
     const uniqueOptions = [];
 
@@ -122,7 +173,7 @@ this.doctype = [
 
     return uniqueOptions;
   }
-  convertTimestamp(timestamp: string) {
+  convertTimestamp(timestamp: string){
     if(timestamp == "" || timestamp == null){
       return timestamp;
     }else{
@@ -132,14 +183,15 @@ this.doctype = [
     const fdate = new Date(sdate);
     return this.datePipe.transform(fdate, 'yyyy-MM-dd');} // Adjust the desired date format
   }
+  getAbsoluteValue(data: number): number {
+    return Math.abs(data);
+  }
  numbertoword(number: number){
   const toWords = new ToWords();
   const words = toWords.convert(number);
   return words;
  }
-
 calculateNetAmount() {
-    //const totalAmounts = this.invoicedata.map(item => item.AmountInTransactionCurrency); // Extract the amounts into an array
     const netAmount = this.invoicedata.reduce((acc, item) => acc + (parseFloat(item.AmountInTransactionCurrency)* -1), 0); // Calculate the sum of amounts
     return netAmount.toFixed(2);
 }
@@ -149,7 +201,6 @@ private generateTableBody(data: number) {
     this.invoicedata = [];
     const invData = this.lis.filter(obj => obj.ClearingAccountingDocument === data && obj.AccountingDocumentType !== 'KZ');
     this.invoicedata = invData;
-    //console.log(this.invoicedata);
     if (this.invoicedata && Array.isArray(this.invoicedata)) {
       this.invoicedata.forEach((item) => {
         const invdate = this.convertTimestamp(item.DocumentDate);
@@ -161,29 +212,35 @@ private generateTableBody(data: number) {
       });
     }
     const table = tableBody;
-      //console.log(tableBody);
     return tableBody;
   }
-
+ 
   previewPDF(item:any, amt:any) {
     const index = this.selectedData.findIndex(x => x.AccountingDocument === item && x.AmountInTransactionCurrency === amt);
-    // const invData = this.lis.filter(obj => obj.ClearingAccountingDocument === item);
-    // this.invoicedata = invData;
     var documentDefinition = {
       pageMargins: [ 30, 30, 30, 30 ],
-      content: [
-      {
-        text: '\nPayment Advise',
-        style: 'header',
-        alignment: 'center'
-        
-      },
+      content: [        
+        {
+          columns: [
+            {
+              width: 100,
+              image: 'logo',
+              bold: true
+            },
+            {
+              width: '*',
+              text: 'Payment Advise',
+              style: 'header',
+              alignment: 'right'
+            }
+          ]
+        },
       "\n",
-          {
+        {
         columns: [
           {
-            width: 200,
-            text: 'To,\n' + this.selectedData[index].SupplierName,
+            width: '*',
+            text: 'To,\n' + this.getsupplier(this.selectedData[index].AccountingDocument),
             fontSize: 11,
           },
           {
@@ -194,16 +251,16 @@ private generateTableBody(data: number) {
           }
         ]
       },
-          {
-              text:"\n 60 Days from the date of GRN.",
-              alignment: 'right'
-          },
-          "\n",
-          {
-              text:"\n Dear Sir / Madam,\nWe have pleasure in transferring through Bank",
-          },
-          "\n",
-          {
+      {
+        text:"\n 60 Days from the date of GRN.",
+        alignment: 'right'
+      },
+      "\n",
+      {
+        text:"\n Dear Sir / Madam,\nWe have pleasure in transferring through Bank",
+      },
+      "\n",
+      {
         columns: [
           {
             width: 200,
@@ -238,65 +295,52 @@ private generateTableBody(data: number) {
           }
         ]
       },
-          "\n",
-      
+      "\n",
       { 
-          table: {
-                  widths: ['*'],
-                  body: [
-                      [
-                          { text: '', alignment: 'center', fontSize: 12, border: [false, false, false, true] },
-                          
-                      ],
-                      
-                  ]
-              }
-          },
-          "\n",
+        table: {
+          widths: ['*'],
+          body: [
+            [
+              { text: '', alignment: 'center', fontSize: 12, border: [false, false, false, true] },            
+            ],        
+          ]
+        }
+      },
+      "\n",
+      {
+        columns: [
           {
-            columns: [
-              {
-                width: '*',
-                text: 'Invoice/DN Date',
-                bold: true,
-                alignment: 'center'
-              },
-              {
-                width: '*',
-                text: 'Invoice/DN No.',
-                bold: true,
-                alignment: 'center'
-              },
-              {
-                width: '*',
-                text: 'Invoice/DN Amount',
-                bold: true,
-                alignment: 'center'
-              },
-              {
-                width: '*',
-                text: 'TDS Amount',
-                bold: true,
-                alignment: 'center'
-              },
-              {
-                width: '*',
-                text: 'Current Payment',
-                bold: true,
-                alignment: 'center'
-              }
-            ]
+            width: '*',
+            text: 'Invoice/DN Date',
+            bold: true,
+            alignment: 'center'
           },
-      // {
-      //   style: 'tableExample',
-      //   table: {
-      //     widths: ['*', '*', '*', '*', '*'],
-      //     body: [
-      //       [{text: 'Invoice/DN Date', style: 'tableHeader'}, {text: 'Invoice/DN No.', style: 'tableHeader'}, {text: 'Invoice/DN Amount', style: 'tableHeader'},  {text: 'TDS Amount', style: 'tableHeader'}, {text: 'Current Payment', style: 'tableHeader'}],
-      //     ]
-      //   },
-      //   layout: 'lightHorizontalLines'
-      // },
+          {
+            width: '*',
+            text: 'Invoice/DN No.',
+            bold: true,
+            alignment: 'center'
+          },
+          {
+            width: '*',
+            text: 'Invoice/DN Amount',
+            bold: true,
+            alignment: 'center'
+          },
+          {
+            width: '*',
+            text: 'TDS Amount',
+            bold: true,
+            alignment: 'center'
+          },
+          {
+            width: '*',
+            text: 'Current Payment',
+            bold: true,
+            alignment: 'center'
+          }
+        ]
+      },
       "\n",
       { 
         table: {
@@ -316,9 +360,7 @@ private generateTableBody(data: number) {
         table: {
           widths: ['*', '*', '*', '*', '*'],
           body: 
-            
             this.generateTableBody(this.selectedData[index].AccountingDocument),
-          
         },
         layout: 'lightHorizontalLines'
       },
@@ -446,7 +488,9 @@ private generateTableBody(data: number) {
         ]
       },
     ],
-  
+    images: {
+      logo: 'https://constructionnarchitecture.com/wp-content/uploads/2018/03/essae.jpg'
+    },
     styles: {
       header: {
         fontSize: 18,
